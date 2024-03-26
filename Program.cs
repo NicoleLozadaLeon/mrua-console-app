80cf3b2a2bad24b7342040f6d08a403f26eed7f6
NicoleLozadaLeon <lozadaleonn@gmail.com>
2024-03-26 15:16:35 -0400

    Añadir kilometros

--- Program.cs
+++ Program.cs
@@ -11,3 +11,8 @@
 
         double distancia = velocidadInicial * tiempo + 0.5 * aceleracion * Math.Pow(tiempo, 2);
         Console.WriteLine($"La distancia recorrida es: {distancia} metros");
+
+        double distancia_km =  distancia / 1000;
+        Console.WriteLine($"La distancia recorrida es: {distancia_km} kilometros");
+
+
